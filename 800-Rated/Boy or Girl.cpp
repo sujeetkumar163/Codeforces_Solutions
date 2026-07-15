d1d3d836154085c96c1a1e223203566121e3f92f
Sujeet Kumar <25U024039@iiitbhopal.ac.in>
2026-07-16 02:00:56 +0530

    236A solved

---
+++ 800-Rated/Boy or Girl.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<set>
+using namespace std;
+int main(){
+   set<char>s;
+   string str;
+   cin>>str;
+   for(int i=0;i<str.length();i++){
+      s.insert(str[i]);
+   }
+
+   int len=s.size();
+   if(len%2==0){
+    cout<<"CHAT WITH HER!"<<endl;
+   }
+   else{
+    cout<<"IGNORE HIM!"<<endl;
+   }
+
+}
